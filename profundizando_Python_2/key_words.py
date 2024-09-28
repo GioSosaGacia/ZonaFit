b56ebe909402522a0a584ba135b8c035ae9d16f9
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-09-28 07:56:22 -0600

    key-words

---
+++ profundizando_Python_2/key_words.py
@@ -0,0 +1,12 @@
+
+'''Palabras reservadas en python'''
+import keyword
+
+print('Palabras reservadas (keywords) en python')
+print(keyword.kwlist)
+
+'''No podemos utilizar keywords para inicializar:
+    variables,
+    funciones,
+    para nombrar archivos ya que se sobreesscribe y no permitir'a importar modulos o librerias
+'''
